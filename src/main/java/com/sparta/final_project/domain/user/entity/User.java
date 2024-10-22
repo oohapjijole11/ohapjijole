b51f22770c0fb223af1ba8e8261fbfa9f99d4731
황우석 <hjldd0523@naver.com>
2024-10-22 19:48:31 +0900

    [FEAT] user엔티티 초기 커밋

--- src/main/java/com/sparta/final_project/domain/user/entity/User.java
+++ src/main/java/com/sparta/final_project/domain/user/entity/User.java
@@ -1,4 +1,48 @@
 package com.sparta.final_project.domain.user.entity;
 
+import com.sparta.final_project.domain.item.entity.Item;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<Item> items;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRating rating;
+
+    // 회원탈퇴 유무
+    private Boolean isdeleted = false;
+
+    public User(String email, String password, String name, UserRole role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+
 }
---
+++ src/main/java/com/sparta/final_project/domain/user/entity/UserRole.java
@@ -0,0 +1,8 @@
+package com.sparta.final_project.domain.user.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER, ADMIN
+}
