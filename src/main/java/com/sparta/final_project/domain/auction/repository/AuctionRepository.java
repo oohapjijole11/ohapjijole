fa2c6df3019247fadb9073805bef40003a1f72df
qowlsrhks <skswlsrhks@naver.com>
2024-10-23 22:45:41 +0900

    [FEAT] _NOT_FOUND_ITEM, _NOT_FOUND_AUCTION 추가

--- src/main/java/com/sparta/final_project/domain/auction/repository/AuctionRepository.java
+++ src/main/java/com/sparta/final_project/domain/auction/repository/AuctionRepository.java
@@ -1,10 +1,23 @@
 package com.sparta.final_project.domain.auction.repository;
 
 import com.sparta.final_project.domain.auction.entity.Auction;
+import com.sparta.final_project.domain.auction.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
+
+    List<Auction> findAllByStatusAndStartTimeLessThan(Status status, LocalDateTime now);
+
+    List<Auction> findAllByStatusAndEndTimeLessThan(Status status, LocalDateTime now);
+
+    List<Auction> findByStatusAndStartTimeLessThan(Status status, LocalDateTime now);
+
+    List<Auction> findByStatusAndEndTimeLessThan(Status status, LocalDateTime now);
+
 }
