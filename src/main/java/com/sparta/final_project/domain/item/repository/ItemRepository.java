5b0940e6d36aecfabd629a2b835982998533d0b8
qowlsrhks <skswlsrhks@naver.com>
2024-10-23 23:11:12 +0900

    [FEAT] item 검증용 repository

---
+++ src/main/java/com/sparta/final_project/domain/item/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.final_project.domain.item.repository;
+
+import com.sparta.final_project.domain.item.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
