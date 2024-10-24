2110e3bde22b688b461b5ab79531a09d40d32d3f
wlsql852@naver.com <wlsql852@naver.com>
2024-10-24 11:43:38 +0900

    [REFACTOR]  Exception 수정

--- src/main/java/com/sparta/final_project/domain/bid/service/SbidService.java
+++ src/main/java/com/sparta/final_project/domain/bid/service/SbidService.java
@@ -8,6 +8,8 @@ import com.sparta.final_project.domain.bid.dto.response.SbidSimpleResponse;
 import com.sparta.final_project.domain.bid.entity.Sbid;
 import com.sparta.final_project.domain.bid.repository.BidRepository;
 import com.sparta.final_project.domain.bid.repository.SbidRepository;
+import com.sparta.final_project.domain.common.exception.ErrorCode;
+import com.sparta.final_project.domain.common.exception.OhapjijoleException;
 import com.sparta.final_project.domain.user.entity.User;
 import com.sparta.final_project.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -30,17 +32,17 @@ public class SbidService {
 
     @Transactional
     public SbidResponse createSbid(Long userId, Long auctionId) {
-        User user = userRepository.findById(userId).orElseThrow(()-> new NullPointerException("해당하는 유저가 존재하지 않습니다."));
-        Auction auction = auctionRepository.findById(auctionId).orElseThrow(()-> new NullPointerException("해당하는 경매가 존재하지 않습니다."));
+        User user = userRepository.findById(userId).orElseThrow(()-> new OhapjijoleException(ErrorCode._USER_NOT_FOUND));
+        Auction auction = auctionRepository.findById(auctionId).orElseThrow(()-> new OhapjijoleException(ErrorCode._NOT_FOUND_AUCTION));
         int maxPrice = bidRepository.findAllByAuctionOrderByCreatedAtDesc(auction).get(0).getPrice();
-        auction.BidSuccess(Status.SUCCESSBID, LocalDateTime.now());
+        auction.bidSuccess(Status.SUCCESSBID, LocalDateTime.now());
         Sbid sbid = new Sbid(user, auction,maxPrice);
         Sbid saveSbid = sbidRepository.save(sbid);
         return new SbidResponse(saveSbid);
     }
 
     public Page<SbidSimpleResponse> getSbids(Long userId, int page, int size) {
-        userRepository.findById(userId).orElseThrow(()-> new NullPointerException("해당하는 유저가 존재하지 않습니다."));
+        userRepository.findById(userId).orElseThrow(()-> new OhapjijoleException(ErrorCode._USER_NOT_FOUND));
         Pageable pageable = PageRequest.of(page-1, size);
         Page<Sbid> sbids = sbidRepository.findAllByUserIdOrderByCreatedAtDesc(userId, pageable);
         return sbids.map(SbidSimpleResponse::new);
