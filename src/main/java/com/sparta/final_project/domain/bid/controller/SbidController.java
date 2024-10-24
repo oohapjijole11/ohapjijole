21de5adc9151c4afd28d417be12e5f8af67293f3
wlsql852@naver.com <wlsql852@naver.com>
2024-10-24 11:48:31 +0900

    [REFACTOR]  pathvariable userId -> @AuthenticationPrincipal AuthUser authUser

--- src/main/java/com/sparta/final_project/domain/bid/controller/SbidController.java
+++ src/main/java/com/sparta/final_project/domain/bid/controller/SbidController.java
@@ -1,9 +1,11 @@
 package com.sparta.final_project.domain.bid.controller;
 
+import com.sparta.final_project.config.security.AuthUser;
 import com.sparta.final_project.domain.bid.dto.response.SbidResponse;
 import com.sparta.final_project.domain.bid.dto.response.SbidSimpleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,11 +19,11 @@ public class SbidController {
         return sbidService.createSbid(userId, auctionId);
     }
 
-    @GetMapping("/purchase/{userId}")
-    public Page<SbidSimpleResponse> getSbid(@PathVariable Long userId,
+    @GetMapping("/purchase")
+    public Page<SbidSimpleResponse> getSbid(@AuthenticationPrincipal AuthUser authUser,
                                             @RequestParam(defaultValue = "1", required = false) int page,
                                             @RequestParam(defaultValue = "10", required = false) int size) {
-        return sbidService.getSbids(userId, page, size);
+        return sbidService.getSbids(authUser.getId(), page, size);
     }
 
 
