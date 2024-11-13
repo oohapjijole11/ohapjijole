581a7972bd1d3e9cf74064fc5e8000f8249d21b7
qowlsrhks <skswlsrhks@naver.com>
2024-11-13 20:44:55 +0900

    [REFACTOR] @NotBlank 추가

--- src/main/java/com/sparta/final_project/domain/auction/dto/request/AuctionRequest.java
+++ src/main/java/com/sparta/final_project/domain/auction/dto/request/AuctionRequest.java
@@ -2,6 +2,7 @@ package com.sparta.final_project.domain.auction.dto.request;
 
 import com.sparta.final_project.domain.auction.entity.Grade;
 import com.sparta.final_project.domain.auction.entity.Status;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -9,9 +10,13 @@ import java.time.LocalDateTime;
 @Getter
 public class AuctionRequest {
 
+    @NotBlank
     private String title;
+    @NotBlank
     private Integer startPrice = 0;
+    @NotBlank
     private LocalDateTime startTime;
+    @NotBlank
     private LocalDateTime endTime;
 
 }
