c60598106b85e9bd6901376d9ace6cb7abb5b590
qowlsrhks <skswlsrhks@naver.com>
2024-11-13 21:01:14 +0900

    [REFACTOR] url 이름 변경

--- src/main/java/com/sparta/final_project/domain/auction/controller/AuctionController.java
+++ src/main/java/com/sparta/final_project/domain/auction/controller/AuctionController.java
@@ -14,13 +14,13 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/auction")
+@RequestMapping("/auctions")
 public class AuctionController {
 
     private final AuctionService auctionService;
 
 //    생성
-    @PostMapping("item/{itemId}")
+    @PostMapping("items/{itemId}")
     public ResponseEntity<AuctionResponse> createAuction(@AuthenticationPrincipal AuthUser authUser, @PathVariable("itemId")Long itemId, @RequestBody AuctionRequest auctionRequest) {
         return ResponseEntity.ok().body(auctionService.createAuction(authUser,itemId,auctionRequest));
     }
@@ -30,7 +30,7 @@ public class AuctionController {
         return ResponseEntity.ok().body(auctionService.getAuction(authUser,auctionId));
     }
 //    다건조회
-    @GetMapping("/auctions")
+    @GetMapping("/")
     public ResponseEntity<List<AuctionResponse>> getAuctionList(@AuthenticationPrincipal AuthUser authUser) {
         return ResponseEntity.ok().body(auctionService.getAuctionList(authUser));
     }
--- src/main/java/com/sparta/final_project/domain/auction/controller/AuctionProgressController.java
+++ src/main/java/com/sparta/final_project/domain/auction/controller/AuctionProgressController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auction")
+@RequestMapping("/auctions")
 public class AuctionProgressController {
 
     private final BidCommonService commonService;
 
 //    경매 시작 알림 메세지
-    @PostMapping("/auction/notify/{auctionId}")
+    @PostMapping("/notify/{auctionId}")
     public ResponseEntity<String> notifyAuctionStart(@PathVariable Long auctionId) {
         commonService.startNotification(auctionId);
         return ResponseEntity.ok("Notification sent to auction " + auctionId);
