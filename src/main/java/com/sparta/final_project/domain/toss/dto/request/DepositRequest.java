ca993a6d00d47067cb5e72558be81c895fc1cfde
qowlsrhks <skswlsrhks@naver.com>
2024-11-13 18:02:34 +0900

    [REMOVE] 가상계좌 삭제

--- src/main/java/com/sparta/final_project/domain/toss/dto/request/DepositRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.final_project.domain.toss.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class DepositRequest {
-
-    private Double amount;
-}
--- src/main/java/com/sparta/final_project/domain/toss/repository/VirtualAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.final_project.domain.toss.repository;
-
-import com.sparta.final_project.domain.toss.entity.VirtualAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VirtualAccountRepository extends JpaRepository<VirtualAccount, Long> {
-    boolean existsByAccountNumber(String accountNumber);
-
-}
