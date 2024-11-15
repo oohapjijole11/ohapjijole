90a79960a1adeebd13d11156042319cfdf1e4f47
nodaji <sniper.nhj@gmail.com>
2024-11-15 15:59:31 +0900

    [FEAT] cicd 파일 생성

--- src/main/java/com/sparta/final_project/FinalProjectApplication.java
+++ src/main/java/com/sparta/final_project/FinalProjectApplication.java
@@ -6,13 +6,19 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Map;
+
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FinalProjectApplication {
-
     public static void main(String[] args) {
+        System.out.println("=== 환경 변수 ===");
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.println(envName + " = " + env.get(envName));
+        }
+
         SpringApplication.run(FinalProjectApplication.class, args);
     }
-
 }
