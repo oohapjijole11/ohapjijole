597032ad9f38750fab59b3a104b94d97a2ed0208
wlsql852@naver.com <wlsql852@naver.com>
2024-11-21 17:12:11 +0900

    [REFACTOR] 티켓 소자지들에게 슬랙 알림 함수 추가

--- src/main/java/com/sparta/final_project/domain/ticket/repository/BuyTicketsRepository.java
+++ src/main/java/com/sparta/final_project/domain/ticket/repository/BuyTicketsRepository.java
@@ -2,10 +2,11 @@ package com.sparta.final_project.domain.ticket.repository;
 
 import com.sparta.final_project.domain.ticket.entity.BuyTickets;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface BuyTicketsRepository extends JpaRepository<BuyTickets, Long> {
     long countByTicketId(Long ticketId);
+
+    List<BuyTickets> findAllByTicketId(Long ticketId);
 }
